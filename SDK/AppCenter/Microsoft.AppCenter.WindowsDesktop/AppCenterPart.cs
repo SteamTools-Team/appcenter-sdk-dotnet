ec9f7c679253ae2ecd6d0388fa1d3c4fd0745a65
Aigio Liu <lysoft.pw@foxmail.com>
2021-03-24 19:44:23 +0800

    fix pub 4.1.0.1

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/AppCenterPart.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/AppCenterPart.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Runtime.Versioning;
+
 namespace Microsoft.AppCenter
 {
+    [SupportedOSPlatform("Windows")]
     public partial class AppCenter
     {
         private const string PlatformIdentifier = "windowsdesktop";
--- SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.Windows.Shared/Crashes.cs
+++ SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.Windows.Shared/Crashes.cs
@@ -11,10 +11,12 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
 namespace Microsoft.AppCenter.Crashes
 {
+    [SupportedOSPlatform("Windows")]
     public partial class Crashes : AppCenterService
     {
         private static readonly object CrashesLock = new object();
